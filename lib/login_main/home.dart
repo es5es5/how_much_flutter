069c3df5d71e6514e334a2e25a88449657fac8ff
es5es5 <es5e5s@nate.com>
2021-01-11 22:25:32 +0900

    Home

--- lib/login_main/home.dart
+++ lib/login_main/home.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:how_much_flutter/login_main/login.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,8 +13,34 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hi'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'How Much ?',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber),
+            ),
+            SizedBox(height: 20),
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://engineering.linecorp.com/wp-content/uploads/2019/08/flutter1.png'),
+              radius: 60,
+            ),
+            SizedBox(height: 20),
+            SignInButton(Buttons.Google,
+                text: 'Sign out',
+                onPressed: () => {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => LoginScreen()))
+                    })
+          ],
+        ),
+      ),
     );
   }
 }
